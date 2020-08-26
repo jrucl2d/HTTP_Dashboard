4e933024a66c97c00041884ef53be27da465b862
jrucl2d <ysk789465@gmail.com>
2020-08-26 23:10:09 +0900

    Register done, Login info to server done

--- app.js
+++ app.js
@@ -2,51 +2,63 @@ const http = require("http");
 const fs = require("fs").promises;
 const PORT = process.env.PORT || 8000;
 
-const server = http
+http
   .createServer(async (req, res) => {
     try {
-      if (req.method === "GET") {
-        if (req.url === "/") {
-          const data = await fs.readFile("./public/index.html");
-          res.end(data);
-        } else if (req.url === "/dashboard") {
-          console.log("왔냐");
-          // 여기서 대시보드 정보 전해줘야 함(이름, 글, 주인이면 수정/삭제)
-        } else {
-          // 정적 파일 제공
-          const data = await fs.readFile(`./public${req.url}`);
-          res.end(data);
-        }
-      } else if (req.method === "POST") {
-        if (req.url === "/login") {
-          let body = "";
-          req.on("data", (data) => (body += data));
-          req.on("end", () => {
-            const { id, password } = JSON.parse(body);
-            // console.log(`${id} ${password} 로그인`);
-            res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-            res.end("good");
-          });
-        } else if (req.url === "/register") {
-          let body = "";
-          req.on("data", (data) => (body += data));
-          req.on("end", async () => {
-            const { id, password } = JSON.parse(body);
-            // console.log(`${id} ${password} 회원가입`);
-            const usersData = await fs.readFile("./database/user.json");
-            const users = JSON.parse(usersData);
-            for (key in users) {
-              if (users[key].id === id && users[key].password === password) {
+      switch (req.method) {
+        case "GET":
+          if (req.url === "/") {
+            const data = await fs.readFile("./public/index.html");
+            res.end(data);
+          } else if (req.url === "/dashboard") {
+            console.log("왔냐");
+            // 여기서 대시보드 정보 전해줘야 함(이름, 글, 주인이면 수정/삭제)
+          } else {
+            // 정적 파일 제공
+            const data = await fs.readFile(`./public${req.url}`);
+            res.end(data);
+          }
+          break;
+        case "POST":
+          if (req.url === "/login") {
+            let body = "";
+            req.on("data", (data) => (body += data));
+            req.on("end", async () => {
+              const { id, password } = JSON.parse(body);
+              // console.log(`${id} ${password} 로그인`);
+              const usersData = await fs.readFile("./database/user.json");
+              const users = JSON.parse(usersData);
+              if (findUser(users, id, password)) {
+                res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+                return res.end("good");
+              }
+              res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+              res.end("no");
+            });
+          } else if (req.url === "/register") {
+            let body = "";
+            req.on("data", (data) => (body += data));
+            req.on("end", async () => {
+              const { id, password } = JSON.parse(body);
+              // console.log(`${id} ${password} 회원가입`);
+              const usersData = await fs.readFile("./database/user.json");
+              const users = JSON.parse(usersData);
+              // 이미 존재하는 계정
+              if (findUser(users, id, password)) {
                 res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
                 return res.end("exist");
               }
-            }
-            users[Date.now()] = { id, password };
-            await fs.writeFile("./database/user.json", JSON.stringify(users));
-            res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
-            res.end("good");
-          });
-        }
+              users[Date.now()] = { id, password };
+              await fs.writeFile("./database/user.json", JSON.stringify(users));
+              res.writeHead(200, { "Content-Type": "text/plain; charset=utf-8" });
+              res.end("good");
+            });
+          }
+          break;
+        case "PUT":
+          break;
+        case "DELETE":
+          break;
       }
     } catch (err) {
       console.error(err);
@@ -55,3 +67,12 @@ const server = http
     }
   })
   .listen(PORT, () => `The server is running on ${PORT}`);
+
+function findUser(users, id, password) {
+  for (key in users) {
+    if (users[key].id === id && users[key].password === password) {
+      return true;
+    }
+  }
+  return false;
+}
--- public/front.js
+++ public/front.js
@@ -1,23 +1,32 @@
 const loginForm = document.querySelector("#loginForm");
 const registerForm = document.querySelector("#registerForm");
 const registerBtn = document.querySelector("#register");
+const loginBtn = document.querySelector("#login");
 
 loginForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   const id = document.querySelector("#id").value;
   const password = document.querySelector("#password").value;
-  // 여기서 아이디와 비밀번호 검사 로직 필요
   if (!id || !password) {
     alert("아이디와 비밀번호를 모두 적어주세요");
     return;
   }
-  await axios.post("/login", { id, password });
+  const loginRes = await axios.post("/login", { id, password });
+  document.querySelector("#id").value = "";
+  document.querySelector("#password").value = "";
+  // 여기서 아이디와 비밀번호 검사 로직 필요
+  if (loginRes.data === "no") {
+    alert("없는 계정입니다. 회원가입을 해주세요");
+    return;
+  }
   const result = await axios.get("/dashboard"); // 여기서 대시보드 정보 받아서 표현해줘야 함
 });
 registerForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   const id = document.querySelector("#regId").value;
   const password = document.querySelector("#regPassword").value;
+  document.querySelector("#regId").value = "";
+  document.querySelector("#regPassword").value = "";
   if (!id || !password) {
     alert("아이디와 비밀번호를 모두 적어주세요");
     return;
@@ -27,14 +36,18 @@ registerForm.addEventListener("submit", async (e) => {
     alert("이미 존재하는 계정입니다");
     return;
   }
+  alert("회원가입을 완료했습니다");
   registerForm.className = "hide";
   loginForm.className = "show";
-  document.querySelector("#regId").value = "";
-  document.querySelector("#regPassword").value = "";
 });
 
 registerBtn.addEventListener("click", (e) => {
   e.preventDefault();
   loginForm.className = "hide";
   registerForm.className = "show";
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  loginForm.className = "show";
+  registerForm.className = "hide";
 });
